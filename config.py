a2a10e08ccf049544f6b737f97006f2ee8183d48
mc0l85 <57678099+mc0l85@users.noreply.github.com>
2025-08-15 13:46:57 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ GENERATE_DEBUG_FILES = True
 TARGET_PRESETS = {
     'qsc': {
         'file_path': 'presets/qsc_target_users.csv',
-        'managers': ['Rodrigo Ferraz', 'Sarah Docker', 'Rhea West', 'Alvaro Cantillo']
+        'managers': ['Sarah Docker', 'Alvaro Cantillo']
     },
     'test': {
-        'managers': ['Satya Nadella']
+        'managers': ['Rodrigo Ferraz', 'Sarah Docker', 'Rhea West', 'Alvaro Cantillo']
+    },
+    'comm': {
+        'managers': ['Rodrigo Ferraz', 'Sarah Docker', 'Rhea West', 'Alvaro Cantillo']
     }
 }
